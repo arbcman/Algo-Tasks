b2f72e07180ac293e990590c9dc4ba94bc8621a2
D0umzz <adamzein461@gmail.com>
2026-05-13 18:52:02 +0200

    3dlt elwiggle sort algorithm

--- Algo_Task1/wigglesort.py
+++ Algo_Task1/wigglesort.py
@@ -83,3 +83,78 @@ Possibly performs one swap -> constant time
 Calls itself with i + 1
 So each call does O(1) -----> T(n)=T(n−1)+O(1) =  O(n)  
 --------------------------------"""
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    # append the rest (if one side length > the other)
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    
+    return result
+
+def newwisort(nums):
+    # using merge sort 
+    ms_nums=merge_sort(nums)
+    n = len(ms_nums)
+    mid = n // 2
+
+    left = nums[:mid][::-1]
+    right = nums[mid:][::-1]
+    
+    result = []
+    i=j=0
+    
+    for k in range(n):
+        if k % 2 == 0:  # even index so (take from left)
+            if i < len(left):
+                result.append(left[i])
+                i += 1
+            elif j < len(right):  # left is exhausted so (take from right)
+                result.append(right[j])
+                j += 1
+        else:  # odd index so (take from right)
+            if j < len(right):
+                result.append(right[j])
+                j += 1
+            elif i < len(left):  # right is exhausted so (take from left)
+                result.append(left[i])
+                i += 1
+                
+    return result
+# Total time complexity
+# Merge sort => O(n log n)
+# Slicing & reversing => O(n)
+# Interleaving => O(n)
+# O(nlogn) + O(n) + O(n) = O(nlogn)
+
+# Dominant term: O(n log n)
+
+
+nums = [1,2,3,4,5,6]
+print("using newwisort: ",newwisort(nums))
+print("using newwisort: ",newwisort([1,3,2,2,3,1]))
+print("using newwisort: ",newwisort([1,5,1,1,6,4]))
+print("using newwisort: ",newwisort([4,4,4,5,5,5]))
