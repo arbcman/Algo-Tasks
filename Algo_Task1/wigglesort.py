e057383fc72dba09fede5069b1e46c14dd2aeaf9
arbcman <3bdulra7man1mr811@gmail.com>
2026-05-13 18:18:08 +0300

    gahza ya haga

--- Algo_Task1/wigglesort.py
+++ Algo_Task1/wigglesort.py
@@ -53,12 +53,13 @@ Time complexity|----> T(n)=T(n−1)+O(1) =  O(n)
 print("Recursive Implementation:")
 
 def reqwisort(arr,i=1):
-    if i>=len(arr):
+    if i>=len(arr):# base case: this is the exit condition that happens (len(arr)) times [n]
         return arr
-    if ((i%2==0 and arr[i] > arr[i-1]) or
-        (i%2==1 and arr[i] < arr[i-1])):
+    # if statment is the main operation to check the [low, high] pattern and will happen (len(arr)) times [n]
+    if ((i%2==0 and arr[i] > arr[i-1]) or#condition to check if the current element is in the correct order
+        (i%2==1 and arr[i] < arr[i-1])):# based on its index (even or odd)
             arr[i],arr[i-1]=arr[i-1],arr[i]
-    return reqwisort(arr,i+1)
+    return reqwisort(arr,i+1)# recursive call: moves to the next index, creating T(n-1) logic
 #///////////////TEST
 print(reqwisort([1,3,2,2,3,1]))
 print(reqwisort([1,5,1,1,6,4]))
@@ -82,6 +83,3 @@ Possibly performs one swap -> constant time
 Calls itself with i + 1
 So each call does O(1) -----> T(n)=T(n−1)+O(1) =  O(n)  
 --------------------------------"""
-
-
-#nums[0] < nums[1] > nums[2] < nums[3]....
