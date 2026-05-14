85ccc739f99d96b15601c1cdd93d4d3d4f9f736b
arbcman <3bdulra7man1mr811@gmail.com>
2026-05-14 06:34:13 +0300

    Last Version

--- Algo_Task1/wigglesort.py
+++ Algo_Task1/wigglesort.py
@@ -1,21 +1,3 @@
-""" 
-----------------------------|
-Psuedocode For Non-Recursive|
-----------------------------|
-
-  FUNCTION wisort(arr):
- //Input: Array of integers
- //Output: Array arranged in [low, high, low, high, ...] pattern
-     FOR i <- 1 TO length(arr) - 1 DO
-         IF (i is even AND arr[i] > arr[i - 1]) OR
-            (i is odd  AND arr[i] < arr[i - 1]) THEN
-               SWAP arr[i] WITH arr[i - 1]
-     RETURN arr
-
----------------|
-Time complexity|----> T(n)=(n−1)⋅O(1)= O(n)
----------------|
-"""
 
 print("Non-Recursive Implementation:")
 
@@ -29,26 +11,10 @@ def wisort(arr):
 #///////////////TEST
 print("using wisort: ",wisort([1,3,2,2,3,1]))
 print("using wisort: ",wisort([1,5,1,1,6,4]))
+
+
 print("---------------------------------------")
-""" 
-------------------------|
-Psuedocode For Recursive|
-------------------------|
 
-  FUNCTION reqwisort(arr,i=1(predefined)):
-    //Input: Array of integers
-    //Output: Array arranged in [low, high, low, high, ...] pattern
-     IF i>=len(arr):
-        RETURN arr
-     IF (i is even AND arr[i] > arr[i - 1]) OR
-           (i is odd  AND arr[i] < arr[i - 1]) THEN
-           SWAP arr[i] WITH arr[i - 1]
-     RETURN reqwisort(arr, i + 1)
-
----------------|
-Time complexity|----> T(n)=T(n−1)+O(1) =  O(n)
----------------|
-"""
 
 print("Recursive Implementation:")
 
@@ -65,21 +31,3 @@ print("using recwisort: ",recwisort([1,3,2,2,3,1]))
 print("using recwisort: ",recwisort([1,5,1,1,6,4]))
 print("---------------------------------------")
 
-
-""" 
---------------------------------
-Comparison of Time Complexities:
---------------------------------
-Both the non-recursive and recursive implementations have the same time complexity of O(n),
-
-The Non-recursive algorithm has a single loop that runs from i = 1 to n - 1
-which means it executes (n − 1) iterations, inside each iteration a Constant number of Operations
-One If condition (the main) Possibly a swap which also constant 
-each iteration is O(1) ----> T(n)=(n−1)⋅O(1)= O(n).
-Similary in the recursive implementation, each recursive call:
-Checks the base case -> constant time
-Evaluates the condition -> constant time
-Possibly performs one swap -> constant time
-Calls itself with i + 1
-So each call does O(1) -----> T(n)=T(n−1)+O(1) =  O(n)  
---------------------------------"""
